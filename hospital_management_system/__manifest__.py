66f9879711e1ddc17829f04e23b43d97bf9a22a6
venkateshmusugu <venkatesh.musugu@sanjittechsolutions.in>
2025-05-21 17:11:14 +0530

    Update __manifest__.py

--- hospital_management_system/__manifest__.py
+++ hospital_management_system/__manifest__.py
@@ -12,7 +12,7 @@
     - Medical Billing
     """,
     'author': "Sanjit Tech",
-    'website': "https://www.Sanjittechsolutions.com",
+    'website': "https://www.sanjittechsolutions.in",
     'category': 'Hospital Management System',
     'version': '17.0.1.0.0',
     'sequence': 5,
